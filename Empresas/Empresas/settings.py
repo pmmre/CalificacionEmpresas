395fa1ba2f8c90e702ed58949099fa905275e769
pmmre <pmmr@correo.ugr.es>
2015-11-15 18:11:32 +0100

    probando despliegue

--- Empresas/Empresas/settings.py
+++ Empresas/Empresas/settings.py
@@ -10,8 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
+ 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ 
+# Allow all host headers
+#ALLOWED_HOSTS = ['*']
 import os
+# Static asset configuration
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,7 +43,7 @@ DEBUG = True
 from unipath import Path
 RUTA_PROYECTO = Path(__file__).ancestor(2)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,14 +92,30 @@ WSGI_APPLICATION = 'Empresas.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+"""
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+"""
+"""
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'pablo',
+        'PASSWORD': 'qwe',
+        'HOST': '\*',
+        'PORT': '5432',
     }
 }
+"""
+DATABASES = {'default' : dj_database_url.config() } 
 
+
+#DATABASES['default'] = dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -104,8 +134,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-	RUTA_PROYECTO.child('static'),
+    RUTA_PROYECTO.child('static'),
 )
